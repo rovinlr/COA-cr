c73612e7ad34a7110e5f10cfc41b5646a2b359b2
rovinlr <rovinlr@gmail.com>
2025-09-12 21:56:23 -0600

    Update __manifest__.py

--- l10n_cr_custom_18_v2/__manifest__.py
+++ l10n_cr_custom_18_v2/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Costa Rica - Contabilidad (Personalizada)",
     "summary": "Paquete de localización CR basado en el formato de Odoo 18 (plantillas).",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.2.0",
     "category": "Accounting/Localizations/Account Charts",
     'countries': ['cr'],
     'url': 'https://github.com/rovinlr/OCA-cr/tree/main/l10n_cr_custom_18_v2',
@@ -10,13 +10,12 @@
     "license": "LGPL-3",
     "depends": ["account"],
     "data": [
-       "data/account_chart_template_data.xml",
-       "data/account_chart_template_configure_data.xml",
-       "data/template/account.account-cr.csv",
+        "data/template/account.account-cr.csv",
         "data/template/account.group-cr.csv",
         "data/template/account.tax.group-cr.csv",
         "data/template/account.tax-cr.csv",
         "data/template/account.fiscal.position-cr.csv",
+        "data/account_chart_template_configure_data.xml"
     ],
     "demo": [],
     "installable": True,
