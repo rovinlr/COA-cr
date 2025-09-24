d45bc23b0e775560a88170cd1a9d540dbad45678
rovinlr <rovinlr@gmail.com>
2025-09-24 05:33:11 -0600

    Update template_cr.py

--- l10n_cr_custom_18_v2/models/template_cr.py
+++ l10n_cr_custom_18_v2/models/template_cr.py
@@ -2,21 +2,21 @@ from odoo import models, _
 from odoo.addons.account.models.chart_template import template
 
 class L10nCRTemplate(models.AbstractModel):
-    _name = 'l10n_cr.chart.template'
+    _name = 'l10n_cr_custom.chart.template'
     _inherit = 'account.chart.template'
 
-    @template('cr')
+    @template('cr_custom')
     def _get_cr_template_data(self):
         # Minimal template data; code_digits set to 7 based on provided CoA
         return {
-            'name': _('Costa Rica - Base'),
+            'name': _('Costa Rica - Custom'),
             'visible': True,
             'code_digits': '7',
             'property_account_receivable_id': 'cr_coa_1040101',
             'property_account_payable_id': 'cr_coa_2010101', 
         }
 
-    @template('cr', 'res.company')
+    @template('cr_custom', 'res.company')
     def _get_cr_res_company(self):
         return {
             self.env.company.id: {
