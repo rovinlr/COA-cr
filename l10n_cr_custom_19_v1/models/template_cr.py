24affd9ed4866e441e87b6fb6386917669ad5764
rovinlr <rovinlr@gmail.com>
2025-09-25 20:27:25 -0600

    Load CR localization templates via @template API

--- l10n_cr_custom_19_v1/models/template_cr.py
+++ l10n_cr_custom_19_v1/models/template_cr.py
@@ -1,9 +1,65 @@
+import csv
+
 from odoo import models, _
 from odoo.addons.account.models.chart_template import template
+from odoo.modules.module import get_module_resource
+
+
+def _convert_csv_value(value):
+    if value in ("True", "False"):
+        return value == "True"
+    try:
+        return int(value)
+    except ValueError:
+        try:
+            return float(value)
+        except ValueError:
+            return value
 
 class L10nCRTemplate(models.AbstractModel):
     _name = 'l10n_cr_custom.chart.template'
     _inherit = 'account.chart.template'
+
+    def _load_template_from_csv(self, relative_path):
+        path = get_module_resource('l10n_cr_custom_19_v1', relative_path)
+        result = {}
+        with open(path, newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            current_id = None
+            for row in reader:
+                record_id = row.get('id') or current_id
+                if not record_id:
+                    continue
+                current_id = record_id
+                values = result.setdefault(record_id, {})
+                row_cache = {}
+                for field_name, raw_value in row.items():
+                    if field_name == 'id' or not raw_value:
+                        continue
+                    self._assign_template_value(values, field_name.split('/'), raw_value, row_cache)
+        return result
+
+    def _assign_template_value(self, container, keys, raw_value, row_cache):
+        if len(keys) == 1:
+            container[keys[0]] = _convert_csv_value(raw_value)
+            return
+
+        head, *tail = keys
+        if head.endswith('_ids'):
+            line_cache = row_cache.get(head)
+            if line_cache is None:
+                line_cache = {}
+                row_cache[head] = line_cache
+                container.setdefault(head, []).append(line_cache)
+            self._assign_template_value(line_cache, tail, raw_value, {})
+            return
+
+        if len(tail) == 1 and tail[0] == 'id':
+            container[head] = _convert_csv_value(raw_value)
+            return
+
+        sub_container = container.setdefault(head, {})
+        self._assign_template_value(sub_container, tail, raw_value, row_cache)
 
     @template('cr_custom', 'account.chart.template')
     def _get_cr_custom_template_data(self):
@@ -23,6 +79,30 @@ class L10nCRTemplate(models.AbstractModel):
             'default_purchase_journal_id': 'cr_custom_purchase_journal',
 
         }
+
+    @template('cr_custom', 'account.group')
+    def _get_cr_custom_account_groups(self):
+        return self._load_template_from_csv('data/template/account.group-cr_custom.csv')
+
+    @template('cr_custom', 'account.account.template')
+    def _get_cr_custom_accounts(self):
+        return self._load_template_from_csv('data/template/account.account-cr_custom.csv')
+
+    @template('cr_custom', 'account.tax.group')
+    def _get_cr_custom_tax_groups(self):
+        return self._load_template_from_csv('data/template/account.tax.group-cr_custom.csv')
+
+    @template('cr_custom', 'account.tax.template')
+    def _get_cr_custom_taxes(self):
+        return self._load_template_from_csv('data/template/account.tax-cr_custom.csv')
+
+    @template('cr_custom', 'account.fiscal.position.template')
+    def _get_cr_custom_fiscal_positions(self):
+        return self._load_template_from_csv('data/template/account.fiscal.position-cr_custom.csv')
+
+    @template('cr_custom', 'account.fiscal.position.tax.template')
+    def _get_cr_custom_fiscal_position_tax_map(self):
+        return self._load_template_from_csv('data/template/account.fiscal.position.tax-cr_custom.csv')
 
     @template('cr_custom', 'account.journal')
     def _get_cr_custom_account_journals(self):
